75ff2651f1a5af4767b318bfb7ca965a10e0ef63
shivumohan <111409304+shivumohan@users.noreply.github.com>
2022-09-21 21:42:09 +0530

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,20 @@
+package com.example.base;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+        String return1(){
+            return "Hello World";
+        }
+
+}
+
+
